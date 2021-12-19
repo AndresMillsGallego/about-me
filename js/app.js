a1253f034aa6ce340b44e07e57cb949fdcc333d8
Andres Mills Gallego <geneseewoodworking@gmail.com>
2021-12-18 18:05:42 -0800

    Final touches! (At least until I take out all those prompts)

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ console.log('Hello Sheyna and Amelia!');
 //Dec 12 changed applicable strings to template literal
 let userName = prompt('Hello! What is your name?');
 alert(`Welcom to my site ${userName}!`);
-alert('Please answer the following questions. (Yes/No answers please');
+alert(`Please answer the following questions. 
+        (Yes/No answers please)`);
 //document.write('Welcome to my site ' +userName+ '!'); I don't like "document.write" There has to be a better way.
 
 //Made 4 arrays actually, 1 for the questions, then another with the corresponding answers.  I decided to make the next list into two arrays so I could really customize the responses. I probably could have kept it in one, but I think its more fun having the responses slightly different.
@@ -32,101 +33,20 @@ function askQuestion() {
   }
 }
 askQuestion();
-// function whereWasIBorn() {
-//   let wasIBornHere = prompt('Was I born in the United States?').toLowerCase();
-//   //console.log(wasIBornHere);
-//   if (wasIBornHere === 'yes' || wasIBornHere === 'y') {
-//     //console.log
-//     alert('That is incorrect.  I was born in Colombia.');
-//   } else if (wasIBornHere === 'no' || wasIBornHere === 'n') {
-//     //console.log('That is correct!  I was born in Colombia.');
-//     alert('That is correct!  I was born in Colombia.');
-//     counter++;
-//   } else {
-//     //console.log('Please enter a Yes or No for your answer.');
-//     alert('Please enter a Yes or No for your answer.');
-//   }
-// }
-// whereWasIBorn();
-
-// function myLanguages() {
-//   let diffLang = prompt('Do I speak another language?').toLowerCase();
-//   if (diffLang === 'yes' || diffLang === 'y') {
-//     //console.log('That is correct!  I also speak Spanish and French.');
-//     alert('That is correct!  I also speak Spanish and French.');
-//     counter++;
-//   } else if (diffLang === 'no' || diffLang === 'n') {
-//     //console.log('That is incorrect.  I also speak Spanish and French.');
-//     alert('That is incorrect.  I also speak Spanish and French.');
-//   } else {
-//     //console.log('Please enter a Yes or No for your answer');
-//     alert('Please enter a Yes or No for your answer');
-//   }
-// }
-// myLanguages();
-
-// function doILikeSushi() {
-//   let favFood = prompt('Do I LOVE Sushi?').toLowerCase();
-//   if (favFood === 'yes' || favFood === 'y') {
-//     //console.log('That is correct!  I love Sushi, it is my favorite food.');
-//     alert('That is correct!  I love Sushi, it is my favorite food.');
-//     counter++;
-//   } else if (favFood === 'no' || favFood === 'n') {
-//     //console.log('That is incorrect.  Sushi is without a doubt my favorite food.');
-//     alert('That is incorrect.  Sushi is without a doubt my favorite food.');
-//   } else {
-//     //console.log('Please enter a Yes or No for your answer');
-//     alert('Please enter a Yes or No for your answer');
-//   }
-// }
-// doILikeSushi();
-
-// function doILoveToTravel() {
-//   let iLoveToTravel = prompt('Do I like traveling?').toLowerCase();
-//   if (iLoveToTravel === 'yes' || iLoveToTravel === 'y') {
-//     //console.log('That is.....almost correct.  I don\'t like traveling, I absolutely LOVE IT!!');
-//     alert('That is.....almost correct.  I don\'t like traveling, I absolutely LOVE IT!!');
-//   } else if (iLoveToTravel === 'no' || iLoveToTravel === 'n') {
-//     //console.log('That is correct!  I don\'t like traveling, I absolutely LOVE IT!!');
-//     alert('That is correct!  I don\'t like traveling, I absolutely LOVE IT!!');
-//     counter++;
-//   } else {
-//     //console.log('Please enter a Yes or No for your answer');
-//     alert('Please enter a Yes or No for your answer');
-//   }
-// }
-// doILoveToTravel();
-
-// function whatIDo() {
-//   let doIWoodwork = prompt('Do you think I know how to use power tools?').toLowerCase();
-//   if (doIWoodwork === 'yes' || doIWoodwork === 'y') {
-//     //console.log('That is correct!  I have been using power tools and working with wood since I was a kid.');
-//     alert('That is correct!  I have been using power tools and working with wood since I was a kid.');
-//     counter++;
-//   } else if (doIWoodwork === 'no' || doIWoodwork === 'n') {
-//     //console.log('That is incorrect.  I have been using power tools and working with wood since I was a kid.');
-//     alert('That is incorrect.  I have been using power tools and working with wood since I was a kid.');
-//   } else {
-//     //console.log('Please enter a Yes or No for your answer');
-//     alert('Please enter a Yes or No for your answer');
-//   }
-// }
-// whatIDo();
 
 alert(`Nice Job! You got ${counter} out of 5 correct so far.`);
 alert('I will give you one for free...I am currently married.');
 
 function yearsMarried() {
   let myMarriageLength = 20;
-  //let marriageGuessCounter = 0; //Going to work on this later....
-  let marriageGuesses = 3; // Working on this now! 12/13/21, glad it worked
+  let marriageGuesses = 3;
   for (let guesses = 0; guesses < 4; guesses++) {
     let howLongHaveIBeenMarried = +prompt('How long do you think I have been married? (Enter number of years)');
     if (howLongHaveIBeenMarried < myMarriageLength) {
       alert(`That is too low.  You have ${marriageGuesses} guesses left.`);
       marriageGuesses--;
     } else if (howLongHaveIBeenMarried > myMarriageLength) {
-      alert(`That is too high.  You have ${marriageGuesses} guessesleft.`);
+      alert(`That is too high.  You have ${marriageGuesses} guesses left.`);
       marriageGuesses--;
     } else if (howLongHaveIBeenMarried === myMarriageLength) {
       alert(`That is just right, and correct!  I have been married for ${myMarriageLength} years.`);
@@ -166,8 +86,6 @@ function myFavoriteFoods() {
   }
 }
 myFavoriteFoods();
-// let favoriteFoodsString = favoriteFoods.join(', ');
-// alert(`Here is a full list of my favorite foods ${favoriteFoodsString}.`); // I did follow the assignment instructions and I did not add this function til after I already turned in Lab 03 (looks so much better now)  :)
 
 // Below I coded 3 different options based on the user's score
 if (counter <= 2) {
